66cc9378223d386c4874208c24c4f64df8a2cc68
Akash Parmar <ak.parmar1442000@gmail.com>
2024-08-01 22:11:53 +0530

    added leva debug in the scene to edit the camera position

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,29 @@
-import { Center, OrbitControls } from "@react-three/drei";
+import { Center, OrbitControls, Clouds, Cloud } from "@react-three/drei";
 import React, { useEffect, useRef, useState } from "react";
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { useControls } from "leva";
-
+import { useLoader } from "@react-three/fiber";
+import * as THREE from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 import Text from "./3DComponents/Text";
 import Skyborg from "./3DComponents/Skyborg";
+import floorMap from "../assets/textures/floorImg.png";
+
 const Experience = () => {
   const skyArr = ["S", "K", "Y"];
   const folioArr = ["F", "O", "L", "I", "O"];
   const skyRef = useRef([]);
   const folioRef = useRef([]);
 
+  const floorTexture = useLoader(TextureLoader, floorMap);
   const [animationState, setAnimationState] = useState("standingUp");
-
+  const { xAxis, yAxis, zAxis } = useControls("skyBorg", {
+    xAxis: { value: 0, min: -10, max: 10, step: 0.01 },
+    yAxis: { value: 0, min: -10, max: 10, step: 0.01 },
+    zAxis: { value: 0, min: -10, max: 10, step: 0.01 },
+  });
   // const { skyPostion } = useControls("sky", {
 
   // });
@@ -23,11 +32,6 @@ const Experience = () => {
   //   yAixs: -2,
   //   zAxis: 0,
   // });
-  const { xAxis, yAxis, zAxis } = useControls("skyBorg", {
-    xAxis: { value: 0, min: -10, max: 10, step: 0.01 },
-    yAixs: { value: 0, min: -10, max: 10, step: 0.01 },
-    zAxis: { value: 0, min: -10, max: 10, step: 0.01 },
-  });
 
   const handleCurrentAnimation = (animType) => {
     setAnimationState(animType);
@@ -62,9 +66,27 @@ const Experience = () => {
   //   }
   // });
 
+  const cameraRef = useRef();
+  const skyBorgRef = useRef();
+  const vec = new THREE.Vector3();
+
+  useFrame(state =>{
+    if(skyBorgRef.current){
+      state.camera.lookAt(skyBorgRef.current.position)
+      state.camera.position.lerp(vec.set(xPosition, yPosition, zPosition), .01)
+      state.camera.updateProjectionMatrix()
+    }
+    return null;
+  })
+
+  useEffect(() => {
+    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+    floorTexture.repeat.set(25, 25); // Adjust the repeat values as needed
+    floorTexture.needsUpdate = true;
+  }, [floorTexture]);
   return (
     <>
-      <OrbitControls makeDefault/>
+      {/* <OrbitControls makeDefault /> */}
       {/* <Center position={[0, 2, 0]}>
         <group>
           {skyArr.map((alphabet, idx) => (
@@ -92,17 +114,24 @@ const Experience = () => {
           ))}
         </group>
       </Center> */}
-      <Center>
-        <Skyborg
-          animationState={animationState}
-          scale={2}
-          position-x={xAxis}
-          position-y={yAxis}
-          position-z={zAxis}
-        />
-      </Center>
+      <Skyborg
+        ref={skyBorgRef}
+        animationState={animationState}
+        scale={2}
+        position-x={xAxis}
+        position-y={yAxis}
+        position-z={zAxis}
+      />
       {/*  position={[skyBorg.xAxis, skyBorg.yAxis, skyBorg.zAxis]} */}
-      <ambientLight intensity={2} />
+      <mesh rotation={[Math.PI * 0.5, 0, 0]}>
+        <planeGeometry args={[1000, 1000, 20, 20]} />
+        <meshStandardMaterial side={THREE.DoubleSide} map={floorTexture} />
+      </mesh>
+      {/* <Clouds material={THREE.MeshBasicMaterial}>
+        <Cloud segments={40} bounds={[10, 2, 2]} volume={10} color="orange" />
+        <Cloud seed={1} scale={2} volume={5} color="hotpink" fade={100} />
+      </Clouds>
+      <ambientLight intensity={2} /> */}
     </>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,71 @@
-import React, { Suspense,useEffect,useRef } from "react";
-import { Canvas,useFrame, useThree  } from "@react-three/fiber";
+import React, { Suspense, useEffect, useRef } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import Experience from "./Experience";
 import { OrbitControls, CameraShake, Environment } from "@react-three/drei";
 import "../App.css";
 import { useControls } from "leva";
+import nx from '../assets/textures/environment/nx.png';
+import ny from '../assets/textures/environment/ny.png';
+import nz from '../assets/textures/environment/nz.png';
+import px from '../assets/textures/environment/px.png';
+import py from '../assets/textures/environment/py.png';
+import pz from '../assets/textures/environment/pz.png';
+import Loading from "./Loading";
+import { gsap } from "gsap";
+
+const CustomCamera = () =>{
+  const {camera} = useThree();
+  const { camX, camY, camZ } = useControls("camera", {
+    camX: { value: 0, min: 0, max: 100, step: 0.1 },
+    camY: { value: 3, min: 0, max: 100, step: 0.1 },
+    camZ: { value: 4, min: 0, max: 100, step: 0.1 },
+    fov: {value: 45, min: 10, max: 100, step: 0.1}
+  });
+  useFrame(() => {
+    if(camera){
+      camera.position.set(camX, camY, camZ);
+    }
+  });
+  useEffect(() => {
+    const animateCamera = () => {
+      const path = [
+        { x: 0, y: 4, z: 10 },
+        { x: 5, y: 10, z: 15 },
+        { x: 10, y: 20, z: 20 },
+      ];
+      gsap.to(camera.position, {
+        duration: 3,
+        repeat: -1,
+        keyframes: path.map(p => ({ x: p.x, y: p.y, z: p.z })),
+        ease: "power1.inOut",
+        onUpdate: () => camera.updateProjectionMatrix(),
+      });
+    };
+
+    animateCamera();
+  }, [camera]);
+  return null;
+}
+
 const Hero = () => {
-  const {camX, camY, camZ} = useControls('camera',{
-    camX: {value:0, min: 0, max: 100, step: 0.1},
-    camY: {value:0, min: 5, max: 100, step: 0.1},
-    camZ: {value:0, min: 0, max: 100, step: 0.1}
-  })
- 
+  
+
   return (
     <section className="hero-container">
       <Canvas
         shadows
         dpr={[1, 2]}
-        camera={{ position: [camX, camY, camZ], fov: 20 }}
+        camera={{ position: [0, 3, 4], fov: 45 }}
       >
         <Suspense fallback={null}>
+          <CustomCamera />
+          <Environment
+            files={[px, nx, py, ny, pz, nz]}
+            background
+            backgroundBlurriness={0} // optional blur factor between 0 and 1 (default: 0, only works with three 0.146 and up)
+            backgroundIntensity={0.5}
+          />
           <Experience />
-          <spotLight position={[50, 50, -30]} castShadow />
-          <pointLight position={[-10, -10, -10]} color="white" intensity={3} />
-          <pointLight position={[0, -5, 5]} intensity={0.5} />
           <directionalLight position={[0, -5, 0]} color="white" intensity={2} />
         </Suspense>
       </Canvas>
