13e95a77628c48c0fee882a8cb8ac6b2d3bf285a
Akash Parmar <ak.parmar1442000@gmail.com>
2024-09-23 23:42:21 +0530

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Navbar from './components/ui/Navbar';
 
 const App = () => {
   return (
     <main id='main-containter'>
+      <Navbar />
       <Hero/>
       <section className='h-screen w-full'></section>
       <section className='h-screen w-full'></section>
---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='fixed z-20 px-4 sm:px-6 md:px-8 lg:px-11 py-4 w-full'>
+        <p className=' top-0 left-0 text-color'>
+            AKASH FOLIO
+        </p>
+    </nav>
+  )
+}
+
+export default Navbar
