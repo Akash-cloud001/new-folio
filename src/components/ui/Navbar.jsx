a9b91cb58c0b220f7ec4b6141c8f4f223137ddb4
Akash Parmar <ak.parmar1442000@gmail.com>
2024-09-24 23:54:22 +0530

    added social logo's in sidebar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useRef, useState } from "react";
 import MenuBarIcon from "../svgComponents/MenuBarIcon";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import Twitter from "../svgComponents/Twitter";
+import Linkedin from "../svgComponents/Linkedin";
+import GithubLogo from "../svgComponents/GithubLogo";
 
 gsap.registerPlugin(useGSAP);
 
@@ -16,25 +19,25 @@ const Navbar = () => {
       name: "about me",
       link: "#about-me",
       classes: "text-8xl tracking-[5px]",
-      anchorClasses: "text-dark-color uppercase",
+      anchorClasses: "text-dark-color uppercase opacity-80",
     },
     {
       name: "journey",
       link: "#journey",
       classes: "text-8xl tracking-[5px]",
-      anchorClasses: "text-dark-color uppercase",
+      anchorClasses: "text-dark-color uppercase opacity-80",
     },
     {
       name: "skills",
       link: "#skills",
       classes: "text-8xl tracking-[5px]",
-      anchorClasses: "text-dark-color uppercase",
+      anchorClasses: "text-dark-color uppercase opacity-80",
     },
     {
       name: "projects",
       link: "#projects",
       classes: "text-8xl tracking-[5px]",
-      anchorClasses: "text-dark-color uppercase",
+      anchorClasses: "text-dark-color uppercase opacity-80",
     },
   ];
 
@@ -112,11 +115,10 @@ const Navbar = () => {
       </figure>
       <div
         ref={sideBarRef}
-        onClick={handleNavOpen}
-        className={`overlay absolute h-screen overflow-hidden z-20 bg-light inset-0 px-4 sm:px-6 md:px-8 lg:px-11 py-4`}
+        className={`overlay absolute h-screen overflow-hidden z-20 bg-light inset-0 px-4 sm:px-6 md:px-8 lg:px-11 py-4 flex justify-between items-center`}
         style={{ transform: "translateX(100%)" }} 
       >
-        <ul className="list-none ff-humane-medium flex flex-col gap-11 items-start justify-center h-full ">
+        <ul className="list-none ff-humane-medium flex flex-col gap-11 items-start justify-center h-max ">
           {navItems.map((item, idx) => (
             <li className={item.classes} key={item.name}>
               <a href={item.link} className={item.anchorClasses}>
@@ -125,6 +127,30 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <ul className="flex gap-8">
+          <li>
+            <a href="#" className="p-[16px] rounded-full button-border-dark flex items-center justify-center">
+              <Twitter className="h-12 w-12 fill-dark-text opacity-80" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="p-[16px] rounded-full button-border-dark flex items-center justify-center">
+              <Linkedin className="h-12 w-12 fill-dark-text opacity-80" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="p-[16px] rounded-full button-border-dark flex items-center justify-center">
+              <GithubLogo className="h-12 w-12 fill-dark-text opacity-80" />
+            </a>
+          </li>
+        </ul>
+
+        <div className="absolute h-32 w-screen -rotate-[18.6deg] bg-primary tracker-container -bottom-10 -right-60">
+          <p>
+            PASSIONATE
+          </p>
+        </div>
       </div>
     </nav>
   );
