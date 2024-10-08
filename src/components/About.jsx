55e55079b162a3edc41c0dea81caa2fedfd480b1
Akash Parmar <ak.parmar1442000@gmail.com>
2024-10-08 23:37:41 +0530

    toggleShader on mobile and desktop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import TextStroke from "./ui/TextStroke";
 import CurveText from "./svgComponents/CurveText";
 import { Canvas } from "@react-three/fiber";
-import ImageShader from './3DComponents/ImageShader'
+import ImageShader from "./3DComponents/ImageShader";
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
@@ -34,71 +34,71 @@ const About = () => {
         duration: 0.75,
       }
     );
-    // gsap.fromTo(
-    //   firstImg.current,
-    //   {
-    //     y: 100,
-    //     opacity: 0,
-    //     scale: 0.7,
-    //   },
-    //   {
-    //     y: 0,
-    //     opacity: 1,
-    //     scale: 1,
-    //     // ease: 'elastic.inOut',
-    //     scrollTrigger: {
-    //       trigger: firstImg.current,
-    //       start: "top 80%",
-    //       end: "bottom bottom",
-    //     },
-    //     duration: 0.5,
-    //   }
-    // );
+    gsap.fromTo(
+      firstImg.current,
+      {
+        y: 100,
+        opacity: 0,
+        scale: 0.7,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        scale: 1,
+        // ease: 'elastic.inOut',
+        scrollTrigger: {
+          trigger: firstImg.current,
+          start: "top 80%",
+          end: "bottom bottom",
+        },
+        duration: 0.5,
+      }
+    );
     gsap.fromTo(
       firstContent.current,
       {
         y: 100,
-        opacity: 0
+        opacity: 0,
       },
       {
         y: 0,
-        opacity:1,
+        opacity: 1,
         scrollTrigger: {
           trigger: aboutContent.current,
           start: "top 60%",
         },
-        delay: 0.2
+        delay: 0.2,
       }
     );
     gsap.fromTo(
       secondContent.current,
       {
         y: 100,
-        opacity: 0
+        opacity: 0,
       },
       {
         y: 0,
-        opacity:1,
+        opacity: 1,
         scrollTrigger: {
           trigger: aboutContent.current,
           start: "top 60%",
         },
-        delay: 0.2
+        delay: 0.2,
       }
     );
     gsap.fromTo(
       secondImg.current,
       {
         y: 100,
-        opacity: 0
+        opacity: 0,
       },
       {
         y: 0,
-        opacity:1,
+        opacity: 1,
         scrollTrigger: {
           trigger: secondImg.current,
           start: "top 70%",
-        }
+        },
       }
     );
   }, []);
@@ -116,23 +116,41 @@ const About = () => {
           </p>
         </figure>
 
-        <section ref={aboutContent} className="flex flex-col lg:flex-row items-start justify-between h-full w-full lg:max-h-[750px] overflow-hidden relative ">
-          <figure ref={firstImg} className=" mx-auto w-full h-[270px] sm:h-[300px] lg:h-[750px] lg:w-1/2 lg:max-w-[600px]">
-            <Canvas dpr={[1,2]} camera={[45, window.innerWidth / window.innerHeight, 0.1, 100]}>
+        <section
+          ref={aboutContent}
+          className="flex flex-col lg:flex-row items-start justify-between h-full w-full lg:max-h-[750px] overflow-hidden relative "
+        >
+          <figure
+            ref={firstImg}
+            className=" mx-auto w-full h-[300px] sm:h-[500px] lg:hidden"
+          >
+            <img
+              className="object-cover w-full h-full"
+              src="./akashInSnow.png"
+              alt="akash in snow image"
+            />
+          </figure>
+          <figure
+            className="hidden lg:block lg:h-[750px] lg:w-1/2 lg:max-w-[600px]"
+          >
+            <Canvas
+              dpr={[1, 2]}
+              camera={[45, window.innerWidth / window.innerHeight, 0.1, 100]}
+            >
               <Suspense>
-                <ImageShader textureSrc={'./akashInSnow.png'}/>
+                <ImageShader textureSrc={"./akashInSnow.png"} />
               </Suspense>
-
             </Canvas>
           </figure>
-          <article className="mt-11 lg:mt-0w-full lg:w-2/5 flex flex-col items-start gap-11 h-[600px] sm:h-[700px] lg:h-[750px] relative">
+          <article className="mt-11 lg:mt-0w-full lg:w-2/5 flex flex-col items-start gap-11 h-[700px] sm:h-[700px] lg:h-[750px] relative">
             <div ref={firstContent}>
               <TextStroke
                 content="Hi,"
                 className="text-5xl sm:text-6xl lg:text-[5rem] xl:text-8xl ff-bold"
               />
               <p className="text-3xl sm:text-4xl lg:text-[44px] ff-bold text-color tracking-wider mt-2 sm:mt-0 xl:mt-4">
-                I'm <span className="secondary-neon"> Akash Parmar</span> <span className="wave">👋🏼</span>
+                I'm <span className="secondary-neon"> Akash Parmar</span>{" "}
+                <span className="wave">👋🏼</span>
               </p>
             </div>
             <div ref={secondContent}>
@@ -140,13 +158,16 @@ const About = () => {
                 content="A Developer"
                 className="text-3xl sm:text-4xl ff-bold text-left "
               />
-              <p className="text-lg sm:text-2xl ff-medium text-color tracking-wider mt-2 text-left ">
+              <p className="text-lg sm:text-2xl ff-medium text-color tracking-wider mt-2 text-left">
                 from India, committed to making easy-to-use and attractive
                 websites.
               </p>
             </div>
 
-            <div ref={secondImg} className="anime-name-container relative mx-auto lg:absolute  bottom-12 lg:right-0">
+            <div
+              ref={secondImg}
+              className="anime-name-container relative mx-auto lg:absolute bottom-4 sm:bottom-0 lg:bottom-12 lg:right-0" 
+            >
               <CurveText />
               <img
                 src="./akashAnime.png"
